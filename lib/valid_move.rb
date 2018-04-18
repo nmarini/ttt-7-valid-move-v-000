55db4757af4a13f137301928a675730eea3d3333
Nicolas Marini <nmarini44@gmail.com>
2018-04-18 00:54:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(array, index)
 if array[index].between?(0, 8) && array[index] == " "|| array[index] =="" || array[index] == nil
   true
-end  
+end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
