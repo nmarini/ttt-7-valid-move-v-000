db493086eb4c94cc4ef4b11cba9c510fef35a658
Nicolas Marini <nmarini44@gmail.com>
2018-04-18 01:02:47 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,6 +6,6 @@ end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(array, position)
+def position_taken?(board, index)
   board[index] != " "
   end
